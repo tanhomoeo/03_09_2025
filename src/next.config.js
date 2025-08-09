b0071d758281972597eb5e9bf16ac2235f92243b
Criminal Investigation Department (CID) <cybercrime.bd.gov@gmail.com>
2025-08-09 13:12:47 +0000

    The NextJS app server is having trouble starting. Please identify what c

---
+++ src/next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.txt$/,
+      use: 'raw-loader',
+    });
+
+    if (isServer) {
+        // Exclude problematic modules from server bundle if they cause issues.
+        config.externals = [...(config.externals || []), '@opentelemetry/winston-transport'];
+    }
+
+    return config;
+  },
+  devIndicators: {
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+};
+
+export default nextConfig;
