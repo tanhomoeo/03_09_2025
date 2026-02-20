5dea33fa07b10ee09056d12a406eb6ec46bee6a6
cybercrime.bd.gov <cybercrime.bd.gov@gmail.com>
2026-02-20 21:52:40 +0000

    loot Le out update

--- src/app/RootLayoutClient.tsx
+++ src/app/RootLayoutClient.tsx
@@ -21,7 +21,7 @@ export default function RootLayoutClient({
   return (
     <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="light"
         enableSystem
         disableTransitionOnChange
       >
